8e97e4d3e340da2da2dbc4e853535d0d587fb45d
eduarda-sofferreira <espf1@aluno.ifal.edu.br>
2024-12-08 14:05:34 -0300

    Create q6.js

---
+++ q6.js
@@ -0,0 +1,8 @@
+var c = 1
+var soma = 0
+while ( c <= 20) {
+    var idade = parseInt (prompt ("informe sua idade"))
+    soma += idade
+    c++
+}
+    console.log("A soma das idades fornecidas é" + soma)
